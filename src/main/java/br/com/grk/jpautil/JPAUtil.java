b00493d2b16e1121b22eb4be1cf4ecb2db60a96c
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2019-10-10 15:16:51 -0300

    Remover/Delete com JPA

--- src/main/java/br/com/grk/jpautil/JPAUtil.java
+++ src/main/java/br/com/grk/jpautil/JPAUtil.java
@@ -19,5 +19,9 @@ public class JPAUtil {
 		return factory.createEntityManager();
 		
 	}
+	
+	public static Object getPrimaryKey(Object entity) {
+		return factory.getPersistenceUnitUtil().getIdentifier(entity);
+	}
 
 }
